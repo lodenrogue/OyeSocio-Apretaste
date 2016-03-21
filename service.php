8ee4e00e0c7bbf4c9436b03f15f7d09dcb26db27
Miguel Hernandez <phyzix5761@gmail.com>
2016-03-21 08:58:10 -0400

    Added Delete post and comments

--- service.php
+++ service.php
@@ -48,23 +48,25 @@ class OyeSocio extends Service
 			$userId = $request->query;
 		}
 
-		// if we have a user id then return that user's profile
+		// if we have a user id then return the target user's profile
 		// Otherwise return the querying user's profile
 		if($userId != null){
-			$output = file_get_contents("http://192.168.1.116:8080/oyesocio/api/profile/{$userId}");
+			$output = file_get_contents("http://192.168.1.116:8080/oyesocio/api/profile/{$userId}?viewerEmail={$request->email}");
 		}
 		else {
-			$output = file_get_contents("http://192.168.1.116:8080/oyesocio/api/profile?email={$request->email}");
+			$output = file_get_contents("http://192.168.1.116:8080/oyesocio/api/profile?viewerEmail={$request->email}&targetEmail={$request->email}");
 		}
 
-		if($output == "ERROR: NO USER FOUND") {
+		if($output == "ERROR: NO USER FOUND WITH EMAIL {$request->email}") {
 			return $this->_main($request);
 		}
-		else if($output == null){
-			//TODO - Let the user know that profile doesn't exist (Use a template)
+		else if($output == "ERROR: NO USER FOUND WITH ID {$userId}"){
+			$response = new Response();
+			$response->setResponseSubject("El perfil no existe");
+			$response->createFromText("Ese perfil no existe en el systema");
+			return $response;
 		}
 		else {
-
 			$response = new Response();
 			$response->setResponseSubject("Perfil");
 			$response->createFromText($output);
@@ -91,6 +93,42 @@ class OyeSocio extends Service
 		}
 	}
 
+	public function _eliminar_publicacion(Request $request){
+		$postId = $request->query;
+
+		$output = null;
+		if(is_numeric($postId)){
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL,"http://192.168.1.116:8080/oyesocio/api/delete-post/{$postId}?userEmail={$request->email}");
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$output = curl_exec ($ch);
+			curl_close ($ch);
+		}
+
+		if($output == "DONE"){
+			$response = new Response();
+			$response->setResponseSubject("Publicacion Eliminada");
+			$response->createFromText("La publicacion ha sido borrada del systema");
+			return $response;
+		}
+		else if($output == "ERROR: USER IS NOT OWNER"){
+			$response = new Response();
+			$response->setResponseSubject("No tiene permiso");
+			$response->createFromText("La publicacion no fue borrada porque usted no tiene permiso para hacer eso");
+			return $response;
+		}
+		else if($output == "ERROR: POST NOT FOUND"){
+			$response = new Response();
+			$response->setResponseSubject("La publicacion no existe");
+			$response->createFromText("La publicacion no fue borrada porque no existe en el systema");
+			return $response;
+		}
+		else {
+			return $this->_main($request);
+		}
+	}
+
 	public function _responder(Request $request){
 		$reply = explode(" ", $request->query, 2);
 		$postId = $reply[0];
@@ -108,11 +146,50 @@ class OyeSocio extends Service
 			return $this->_main($request);
 		}
 		else if($serOutput == "ERROR: NO POST FOUND"){
-			//TODO - Let the user know the post doesn't exist (Use a template)
+			$response = new Response();
+			$response->setResponseSubject("La publicacion no existe");
+			$response->createFromText("La repuesta no fue submitida porque la publicacion no existe en el systema");
+			return $response;
 		}
 		else {
 			$outputJson = json_decode($serverOutput);
 			return $this->_perfil($request, $outputJson->userId);
+		}
+	}
+
+	public function _eliminar_repuesta(Request $request){
+		$commentId = $request->query;
+
+		$output = null;
+		if(is_numeric($commentId)){
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL,"http://192.168.1.116:8080/oyesocio/api/delete-comment/{$commentId}?userEmail={$request->email}");
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$output = curl_exec ($ch);
+			curl_close ($ch);
+		}
+
+		if($output == "DONE"){
+			$response = new Response();
+			$response->setResponseSubject("Repuesta Eliminada");
+			$response->createFromText("La repuesta ha sido borrada del systema");
+			return $response;
+		}
+		else if($output == "ERROR: USER IS NOT OWNER"){
+			$response = new Response();
+			$response->setResponseSubject("No tiene permiso");
+			$response->createFromText("La repuesta no fue borrada porque usted no tiene permiso para hacer eso");
+			return $response;
+		}
+		else if($output == "ERROR: COMMENT NOT FOUND"){
+			$response = new Response();
+			$response->setResponseSubject("La repuesta no existe");
+			$response->createFromText("La repuesta no fue borrada porque no existe en el systema");
+			return $response;
+		}
+		else {
+			return $this->_main($request);
 		}
 	}
 }
